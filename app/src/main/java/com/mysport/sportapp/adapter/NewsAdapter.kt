c6ec106d9bd4bbf98f2c58b809fc47db495bd5e7
Naufal Prima Yoriko <primayoriko@gmail.com>
2021-04-17 02:41:29 +0700

    chore: add bind in newsviewholder

--- app/src/main/java/com/mysport/sportapp/adapter/NewsAdapter.kt
+++ app/src/main/java/com/mysport/sportapp/adapter/NewsAdapter.kt
@@ -1,20 +1,27 @@
 package com.mysport.sportapp.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.mysport.sportapp.R
+import com.mysport.sportapp.databinding.NewsItemBinding
 import com.mysport.sportapp.model.News
 
-class NewsAdapter (val news: ArrayList<News>):
+class NewsAdapter (val newsList: ArrayList<News>):
     RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
-    override fun getItemCount(): Int = news.size
-
-    override fun onBindViewHolder(holder: NewsViewHolder, position: Int){
-
-    }
+    override fun getItemCount(): Int = newsList.size
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.news_item, parent, false)
+
+        return NewsViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
+        holder.bind(newsList[position])
     }
 
     class NewsViewHolder(itemView: View):
@@ -23,7 +30,7 @@ class NewsAdapter (val news: ArrayList<News>):
         private var news: News? = null
 
         init {
-
+            itemView.setOnClickListener(this)
         }
 
         override fun onClick(v: View?) {
@@ -32,6 +39,20 @@ class NewsAdapter (val news: ArrayList<News>):
 
         fun bind(news: News){
             this.news = news
+
+            val imageUrl = StringBuilder()
+
+            imageUrl.append(news.urlToImage)
+
+            val newsItemBinding: NewsItemBinding = NewsItemBinding.bind(view)
+//                    NewsItemBinding.inflate(
+//                            LayoutInflater.from(view.context),
+//                            view.parent as ViewGroup?,
+//                            false
+//                    )
+            newsItemBinding.newsTitle.text = news.title
+            newsItemBinding.newsAuthor.text = news.author
+            Glide.with(view.context).load(imageUrl.toString()).into(newsItemBinding.newsImage)
 
         }
     }
