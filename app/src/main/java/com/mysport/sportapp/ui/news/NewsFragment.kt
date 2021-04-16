6ea9ac49fd9a2fff692ffb2f640beec9d5a46ceb
Naufal Prima Yoriko <primayoriko@gmail.com>
2021-04-17 01:39:40 +0700

    chore: design news_item

--- app/src/main/java/com/mysport/sportapp/ui/news/NewsFragment.kt
+++ app/src/main/java/com/mysport/sportapp/ui/news/NewsFragment.kt
@@ -1,20 +1,36 @@
 package com.mysport.sportapp.ui.news
 
+import android.content.res.Resources
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil.inflate
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.mysport.sportapp.R
+import com.mysport.sportapp.adapter.NewsAdapter
+import java.util.zip.Inflater
 
 class NewsFragment: Fragment() {
     private lateinit var newsViewModel: NewsViewModel
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View?{
+        val gridColumnCount: Int = resources.getInteger(R.integer.grid_column_count)
 
-        return null
+        val view: View = inflater.inflate(R.layout.fragment_news, container, false)
+
+        recyclerView = view.findViewById(R.id.recyclerview)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = GridLayoutManager(view.context, gridColumnCount);
+//        recyclerView.adapter = NewsAdapter();
+
+        return view
     }
 }
