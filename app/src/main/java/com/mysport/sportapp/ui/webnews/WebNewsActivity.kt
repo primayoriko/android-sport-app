3bea978b67cf30f4b54a69a4482b78ace9c7253c
Naufal Prima Yoriko <primayoriko@gmail.com>
2021-04-19 01:47:32 +0700

    feat: add webview on news

--- app/src/main/java/com/mysport/sportapp/ui/webnews/WebNewsActivity.kt
+++ app/src/main/java/com/mysport/sportapp/ui/webnews/WebNewsActivity.kt
@@ -1,21 +1,35 @@
 package com.mysport.sportapp.ui.webnews
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 import com.mysport.sportapp.R
+import kotlinx.android.synthetic.main.activity_web_news.*
+import kotlinx.android.synthetic.main.content_web_news.*
 
 class WebNewsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web_news)
-        setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        setSupportActionBar(webNewsToolbar)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        supportActionBar!!.setDisplayShowHomeEnabled(true)
+
+        val url: String? = intent.getStringExtra("url")
+
+        webViewNews.loadUrl(url!!)
+        webViewNews.webViewClient = WebViewClient()
+
+//        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show()
+//        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
     }
 }
