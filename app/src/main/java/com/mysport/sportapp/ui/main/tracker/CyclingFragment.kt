b0e7fa0d2cf36e824e255edf5bd04637d54077d3
Naufal Prima Yoriko <primayoriko@gmail.com>
2021-04-25 10:18:28 +0700

    chore: create CyclingFragment

--- app/src/main/java/com/mysport/sportapp/ui/main/tracker/CyclingFragment.kt
+++ app/src/main/java/com/mysport/sportapp/ui/main/tracker/CyclingFragment.kt
@@ -5,15 +5,25 @@ import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.LatLngBounds
-import com.google.android.gms.maps.model.Polyline
 import com.google.android.gms.maps.model.PolylineOptions
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.snackbar.Snackbar
 import com.mysport.sportapp.R
+import com.mysport.sportapp.data.Constant.ACTION_PAUSE_SERVICE
+import com.mysport.sportapp.data.Constant.ACTION_START_OR_RESUME_SERVICE
+import com.mysport.sportapp.data.Constant.ACTION_STOP_SERVICE
+import com.mysport.sportapp.data.Constant.MAP_ZOOM
+import com.mysport.sportapp.data.Constant.POLYLINE_COLOR
+import com.mysport.sportapp.data.Constant.POLYLINE_WIDTH
+import com.mysport.sportapp.service.CyclingService
+import com.mysport.sportapp.util.TrackerUtility
+import com.mysport.sportapp.data.Polyline
+import kotlinx.android.synthetic.main.fragment_cycling.*
 import java.util.*
 import kotlin.math.round
 
@@ -64,139 +74,139 @@ class CyclingFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-//        mapView.onCreate(savedInstanceState)
-//        btnToggleRun.setOnClickListener {
-//            toggleRun()
-//        }
-//
-//        btnFinishRun.setOnClickListener {
-//            zoomToSeeWholeTrack()
-//            endRunAndSaveToDb()
-//        }
-//
-//        mapView.getMapAsync {
-//            map = it
-//            addAllPolylines()
-//        }
-//
-//        subscribeToObservers()
+        mapView.onCreate(savedInstanceState)
+        btnToggleRun.setOnClickListener {
+            toggleRun()
+        }
+
+        btnFinishRun.setOnClickListener {
+            zoomToSeeWholeTrack()
+            endRunAndSaveToDb()
+        }
+
+        mapView.getMapAsync {
+            map = it
+            addAllPolylines()
+        }
+
+        subscribeToObservers()
     }
 
-//    private fun subscribeToObservers() {
-//        TrackingService.isTracking.observe(viewLifecycleOwner, Observer {
-//            updateTracking(it)
-//        })
-//
-//        TrackingService.pathPoints.observe(viewLifecycleOwner, Observer {
-//            pathPoints = it
-//            addLatestPolyline()
-//            moveCameraToUser()
-//        })
-//
-//        TrackingService.timeRunInMillis.observe(viewLifecycleOwner, Observer {
-//            curTimeInMillis = it
-//            val formattedTime = TrackingUtility.getFormattedStopWatchTime(curTimeInMillis, true)
-//            tvTimer.text = formattedTime
-//        })
-//    }
-//
-//    private fun toggleRun() {
-//        if(isTracking) {
-//            menu?.getItem(0)?.isVisible = true
-//            sendCommandToService(ACTION_PAUSE_SERVICE)
-//        } else {
-//            sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
-//        }
-//    }
-//
-//    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-//        super.onCreateOptionsMenu(menu, inflater)
-//        inflater.inflate(R.menu.toolbar_tracking_menu, menu)
-//        this.menu = menu
-//    }
-//
-//    override fun onPrepareOptionsMenu(menu: Menu) {
-//        super.onPrepareOptionsMenu(menu)
-//        if(curTimeInMillis > 0L) {
-//            this.menu?.getItem(0)?.isVisible = true
-//        }
-//    }
-//
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        when(item.itemId) {
-//            R.id.miCancelTracking -> {
-//                showCancelTrackingDialog()
-//            }
-//        }
-//        return super.onOptionsItemSelected(item)
-//    }
-//
-//    private fun showCancelTrackingDialog() {
-//        val dialog = MaterialAlertDialogBuilder(requireContext(), R.style.AlertDialogTheme)
-//                .setTitle("Cancel the Run?")
-//                .setMessage("Are you sure to cancel the current run and delete all its data?")
-//                .setIcon(R.drawable.ic_delete)
-//                .setPositiveButton("Yes") { _, _ ->
-//                    stopRun()
-//                }
-//                .setNegativeButton("No") { dialogInterface, _ ->
-//                    dialogInterface.cancel()
-//                }
-//                .create()
-//        dialog.show()
-//    }
-//
-//    private fun stopRun() {
-//        sendCommandToService(ACTION_STOP_SERVICE)
+    private fun subscribeToObservers() {
+        CyclingService.isTracking.observe(viewLifecycleOwner, Observer {
+            updateTracking(it)
+        })
+
+        CyclingService.pathPoints.observe(viewLifecycleOwner, Observer {
+            pathPoints = it
+            addLatestPolyline()
+            moveCameraToUser()
+        })
+
+        CyclingService.timeRunInMillis.observe(viewLifecycleOwner, Observer {
+            curTimeInMillis = it
+            val formattedTime = TrackerUtility.getFormattedStopWatchTime(curTimeInMillis, true)
+            tvTimer.text = formattedTime
+        })
+    }
+
+    private fun toggleRun() {
+        if(isTracking) {
+            menu?.getItem(0)?.isVisible = true
+            sendCommandToService(ACTION_PAUSE_SERVICE)
+        } else {
+            sendCommandToService(ACTION_START_OR_RESUME_SERVICE)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.toolbar_tracker_menu, menu)
+        this.menu = menu
+    }
+
+    override fun onPrepareOptionsMenu(menu: Menu) {
+        super.onPrepareOptionsMenu(menu)
+        if(curTimeInMillis > 0L) {
+            this.menu?.getItem(0)?.isVisible = true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.miCancelTrack -> {
+                showCancelTrackingDialog()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showCancelTrackingDialog() {
+        val dialog = MaterialAlertDialogBuilder(requireContext(), R.style.AlertDialogTheme)
+                .setTitle("Cancel the Run?")
+                .setMessage("Are you sure to cancel the current run and delete all its data?")
+                .setIcon(R.drawable.ic_white_delete_24)
+                .setPositiveButton("Yes") { _, _ ->
+                    stopRun()
+                }
+                .setNegativeButton("No") { dialogInterface, _ ->
+                    dialogInterface.cancel()
+                }
+                .create()
+        dialog.show()
+    }
+
+    private fun stopRun() {
+        sendCommandToService(ACTION_STOP_SERVICE)
 //        findNavController().navigate(R.id.action_trackingFragment_to_runFragment)
-//    }
-//
-//    private fun updateTracking(isTracking: Boolean) {
-//        this.isTracking = isTracking
-//        if(!isTracking) {
-//            btnToggleRun.text = "Start"
-//            btnFinishRun.visibility = View.VISIBLE
-//        } else {
-//            btnToggleRun.text = "Stop"
-//            menu?.getItem(0)?.isVisible = true
-//            btnFinishRun.visibility = View.GONE
-//        }
-//    }
-//
-//    private fun moveCameraToUser() {
-//        if(pathPoints.isNotEmpty() && pathPoints.last().isNotEmpty()) {
-//            map?.animateCamera(
-//                    CameraUpdateFactory.newLatLngZoom(
-//                            pathPoints.last().last(),
-//                            MAP_ZOOM
-//                    )
-//            )
-//        }
-//    }
-//
-//    private fun zoomToSeeWholeTrack() {
-//        val bounds = LatLngBounds.Builder()
-//        for(polyline in pathPoints) {
-//            for(pos in polyline) {
-//                bounds.include(pos)
-//            }
-//        }
-//
-//        map?.moveCamera(
-//                CameraUpdateFactory.newLatLngBounds(
-//                        bounds.build(),
-//                        mapView.width,
-//                        mapView.height,
-//                        (mapView.height * 0.05f).toInt()
-//                )
-//        )
-//    }
-//
-//    private fun endRunAndSaveToDb() {
+    }
+
+    private fun updateTracking(isTracking: Boolean) {
+        this.isTracking = isTracking
+        if(!isTracking) {
+            btnToggleRun.text = "Start"
+            btnFinishRun.visibility = View.VISIBLE
+        } else {
+            btnToggleRun.text = "Stop"
+            menu?.getItem(0)?.isVisible = true
+            btnFinishRun.visibility = View.GONE
+        }
+    }
+
+    private fun moveCameraToUser() {
+        if(pathPoints.isNotEmpty() && pathPoints.last().isNotEmpty()) {
+            map?.animateCamera(
+                    CameraUpdateFactory.newLatLngZoom(
+                            pathPoints.last().last(),
+                            MAP_ZOOM
+                    )
+            )
+        }
+    }
+
+    private fun zoomToSeeWholeTrack() {
+        val bounds = LatLngBounds.Builder()
+        for(polyline in pathPoints) {
+            for(pos in polyline) {
+                bounds.include(pos)
+            }
+        }
+
+        map?.moveCamera(
+                CameraUpdateFactory.newLatLngBounds(
+                        bounds.build(),
+                        mapView.width,
+                        mapView.height,
+                        (mapView.height * 0.05f).toInt()
+                )
+        )
+    }
+
+    private fun endRunAndSaveToDb() {
 //        map?.snapshot { bmp ->
 //            var distanceInMeters = 0
 //            for(polyline in pathPoints) {
-//                distanceInMeters += TrackingUtility.calculatePolylineLength(polyline).toInt()
+//                distanceInMeters += TrackerUtility.calculatePolylineLength(polyline).toInt()
 //            }
 //            val avgSpeed = round((distanceInMeters / 1000f) / (curTimeInMillis / 1000f / 60 / 60) * 10) / 10f
 //            val dateTimestamp = Calendar.getInstance().timeInMillis
@@ -210,84 +220,85 @@ class CyclingFragment : Fragment() {
 //            ).show()
 //            stopRun()
 //        }
-//    }
-//
-//    private fun addAllPolylines() {
-//        for(polyline in pathPoints) {
-//            val polylineOptions = PolylineOptions()
-//                    .color(POLYLINE_COLOR)
-//                    .width(POLYLINE_WIDTH)
-//                    .addAll(polyline)
-//            map?.addPolyline(polylineOptions)
-//        }
-//    }
-//
-//    private fun addLatestPolyline() {
-//        if(pathPoints.isNotEmpty() && pathPoints.last().size > 1) {
-//            val preLastLatLng = pathPoints.last()[pathPoints.last().size - 2]
-//            val lastLatLng = pathPoints.last().last()
-//            val polylineOptions = PolylineOptions()
-//                    .color(POLYLINE_COLOR)
-//                    .width(POLYLINE_WIDTH)
-//                    .add(preLastLatLng)
-//                    .add(lastLatLng)
-//            map?.addPolyline(polylineOptions)
-//        }
-//    }
-//
-//    private fun sendCommandToService(action: String) =
-//            Intent(requireContext(), TrackingService::class.java).also {
-//                it.action = action
-//                requireContext().startService(it)
-//            }
-//
-//    override fun onResume() {
-//        super.onResume()
-//        mapView?.onResume()
-//    }
-//
-//    override fun onStart() {
-//        super.onStart()
-//        mapView?.onStart()
-//    }
-//
-//    override fun onStop() {
-//        super.onStop()
-//        mapView?.onStop()
-//    }
-//
-//    override fun onPause() {
-//        super.onPause()
-//        mapView?.onPause()
-//    }
-//
-//    override fun onLowMemory() {
-//        super.onLowMemory()
-//        mapView?.onLowMemory()
-//    }
-//
-//    override fun onSaveInstanceState(outState: Bundle) {
-//        super.onSaveInstanceState(outState)
-//        mapView?.onSaveInstanceState(outState)
-//    }
+        stopRun()
+    }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CyclingFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                CyclingFragment().apply {
+    private fun addAllPolylines() {
+        for(polyline in pathPoints) {
+            val polylineOptions = PolylineOptions()
+                    .color(POLYLINE_COLOR)
+                    .width(POLYLINE_WIDTH)
+                    .addAll(polyline)
+            map?.addPolyline(polylineOptions)
+        }
+    }
+
+    private fun addLatestPolyline() {
+        if(pathPoints.isNotEmpty() && pathPoints.last().size > 1) {
+            val preLastLatLng = pathPoints.last()[pathPoints.last().size - 2]
+            val lastLatLng = pathPoints.last().last()
+            val polylineOptions = PolylineOptions()
+                    .color(POLYLINE_COLOR)
+                    .width(POLYLINE_WIDTH)
+                    .add(preLastLatLng)
+                    .add(lastLatLng)
+            map?.addPolyline(polylineOptions)
+        }
+    }
+
+    private fun sendCommandToService(action: String) =
+            Intent(requireContext(), CyclingService::class.java).also {
+                it.action = action
+                requireContext().startService(it)
+            }
+
+    override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView?.onSaveInstanceState(outState)
+    }
+
+//    companion object {
+//        /**
+//         * Use this factory method to create a new instance of
+//         * this fragment using the provided parameters.
+//         *
+//         * @param param1 Parameter 1.
+//         * @param param2 Parameter 2.
+//         * @return A new instance of fragment CyclingFragment.
+//         */
+//        // TODO: Rename and change types and number of parameters
+//        @JvmStatic
+//        fun newInstance(param1: String, param2: String) =
+//                CyclingFragment().apply {
 //                    arguments = Bundle().apply {
 //                        putString(ARG_PARAM1, param1)
 //                        putString(ARG_PARAM2, param2)
 //                    }
-                }
-    }
+//                }
+//    }
 }
