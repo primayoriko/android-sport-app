aede212f106ede5c47b39c599bbe0115322e0448
Naufal Prima Yoriko <primayoriko@gmail.com>
2021-04-27 20:45:59 +0700

    feat: create compass functionality for trackerfragment

--- app/src/main/java/com/mysport/sportapp/ui/main/tracker/TrackerFragment.kt
+++ app/src/main/java/com/mysport/sportapp/ui/main/tracker/TrackerFragment.kt
@@ -1,31 +1,41 @@
 package com.mysport.sportapp.ui.main.tracker
 
 import android.Manifest
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.mysport.sportapp.R
 import com.mysport.sportapp.data.Constant.REQUEST_CODE_LOCATION_PERMISSION
-import com.mysport.sportapp.databinding.FragmentTrackerBinding
 import com.mysport.sportapp.util.TrackerUtility
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_tracker.*
 import pub.devrel.easypermissions.AppSettingsDialog
 import pub.devrel.easypermissions.EasyPermissions
 
-
 @AndroidEntryPoint
-class TrackerFragment : Fragment(), EasyPermissions.PermissionCallbacks {
-//    private val binding: FragmentTrackerBinding
+class TrackerFragment : Fragment(),
+        EasyPermissions.PermissionCallbacks, SensorEventListener {
+
+    //    private val binding: FragmentTrackerBinding
+    private lateinit var sensorManager: SensorManager
+    private lateinit var accelerometerSensor: Sensor
+    private lateinit var magneticFieldSensor: Sensor
+
+    private var floatGravity = FloatArray(3)
+    private var floatGeoMagnetic = FloatArray(3)
+
+    private var floatOrientation = FloatArray(3)
+    private var floatRotationMatrix = FloatArray(9)
 
     companion object {
         fun newInstance() = TrackerFragment()
@@ -33,9 +43,14 @@ class TrackerFragment : Fragment(), EasyPermissions.PermissionCallbacks {
 
     private lateinit var viewModel: TrackerViewModel
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
 
 //        val spinner: Spinner? =  view.findViewById(R.id.dropdownTracker)
@@ -74,12 +89,54 @@ class TrackerFragment : Fragment(), EasyPermissions.PermissionCallbacks {
         }
 
         requestPermissions()
+        activateSensors()
+
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
         viewModel = ViewModelProvider(this).get(TrackerViewModel::class.java)
-        // TODO: Use the ViewModel
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        if(event.sensor?.type == Sensor.TYPE_ACCELEROMETER){
+            floatGravity = event.values;
+
+        } else if(event.sensor?.type == Sensor.TYPE_MAGNETIC_FIELD){
+            floatGeoMagnetic = event.values;
+
+        }
+
+        SensorManager.getRotationMatrix(floatRotationMatrix, null, floatGravity, floatGeoMagnetic);
+        SensorManager.getOrientation(floatRotationMatrix, floatOrientation);
+
+        imageCompass?.rotation = (-floatOrientation[0] * 180 / 3.14159).toFloat()
+
+//        Timber.d(floatOrientation[0].toString())
+//        Timber.d(floatOrientation[1].toString())
+//        Timber.d(floatOrientation[2].toString())
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {}
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
+        if(EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
+            AppSettingsDialog.Builder(this).build().show()
+        } else {
+            requestPermissions()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<out String>,
+            grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
     }
 
     private fun requestPermissions() {
@@ -117,26 +174,16 @@ class TrackerFragment : Fragment(), EasyPermissions.PermissionCallbacks {
                 // TODO: Search permission needed
             }
         }
-
     }
 
-    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>) {
-        if(EasyPermissions.somePermissionPermanentlyDenied(this, perms)) {
-            AppSettingsDialog.Builder(this).build().show()
-        } else {
-            requestPermissions()
-        }
-    }
+    private fun activateSensors(){
+        sensorManager = requireActivity().getSystemService(Context.SENSOR_SERVICE) as SensorManager
 
-    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {}
+        accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        magneticFieldSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
 
-    override fun onRequestPermissionsResult(
-            requestCode: Int,
-            permissions: Array<out String>,
-            grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this)
+        sensorManager.registerListener(this, magneticFieldSensor, 0)
+        sensorManager.registerListener(this, accelerometerSensor, 0)
     }
 
 }
