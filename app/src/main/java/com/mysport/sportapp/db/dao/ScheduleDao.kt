e681f6e7dd3119d74990a39c25bc08849dda06e8
Naufal Prima Yoriko <primayoriko@gmail.com>
2021-04-30 08:43:10 +0700

    chore: add schedule dao and repo methods

--- app/src/main/java/com/mysport/sportapp/db/dao/ScheduleDao.kt
+++ app/src/main/java/com/mysport/sportapp/db/dao/ScheduleDao.kt
@@ -11,21 +11,26 @@ interface ScheduleDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(schedule: Schedule)
 
+    @Update
+    suspend fun update(schedule: Schedule)
+
     @Delete
     suspend fun delete(schedule: Schedule)
+
+    @Query("SELECT * FROM schedule WHERE id = :id")
+    fun get(id: Int): LiveData<Schedule>
+
+    @Query("SELECT * FROM schedule ORDER BY id DESC")
+    fun getAllSortedByID(): LiveData<List<Schedule>>
+
+    @Query("SELECT * FROM schedule ORDER BY isActive DESC")
+    fun getAllSortedByStatus(): LiveData<List<Schedule>>
 
     // TODO: Order by time or type
     @Query("SELECT * FROM schedule ORDER BY id DESC")
     fun getAllSortedByDate(): LiveData<List<Schedule>>
 
-    @Query("SELECT * FROM schedule ORDER BY id DESC")
-    fun getAllSortedByID(): LiveData<List<Schedule>>
-
-    @Update
-    fun update(schedule: Schedule)
-
-    // Examples
-//    @Query("SELECT * FROM running_table ORDER BY timestamp DESC")
-//    fun getAllRunsSortedByDate(): LiveData<List<Run>>
+    @Query("SELECT * FROM schedule WHERE isActive = 1 ORDER BY id DESC")
+    fun getAllActive(): LiveData<List<Schedule>>
 
 }
