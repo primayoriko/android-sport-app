703c71452220383335d0e93121247b99d11d842a
Naufal Prima Yoriko <primayoriko@gmail.com>
2021-04-30 11:00:41 +0700

    fix: bug of splitting db

--- app/src/main/java/com/mysport/sportapp/db/MainDatabase.kt
+++ app/src/main/java/com/mysport/sportapp/db/MainDatabase.kt
@@ -4,15 +4,18 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.mysport.sportapp.data.Schedule
+import com.mysport.sportapp.data.Training
 import com.mysport.sportapp.db.dao.ScheduleDao
+import com.mysport.sportapp.db.dao.TrainingDao
 import com.mysport.sportapp.util.Converter
 
 @Database(
-        entities = [Schedule::class],
-        version = 1
+    entities = [Training::class, Schedule::class],
+    version = 1
 )
 @TypeConverters(Converter::class)
-abstract class ScheduleDatabase: RoomDatabase() {
+abstract class MainDatabase: RoomDatabase() {
 
-    abstract fun getScheduleDao(): ScheduleDao
+    abstract fun getTrainingDao(): TrainingDao
+    abstract  fun getScheduleDao(): ScheduleDao
 }
